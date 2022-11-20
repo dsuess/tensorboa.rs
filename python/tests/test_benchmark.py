1b7181ea34dbc66584d0f5969217f7412b742bf6
Daniel Suess <daniel@dsuess.me>
2022-11-20 19:13:17 +1100

    Implement image-decoding test

--- python/tests/test_benchmark.py
+++ python/tests/test_benchmark.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+import numpy as np
 import pytest as pt
 import tensorboardX as tbx
 from tensorboard.backend.event_processing.event_accumulator import EventAccumulator
@@ -32,7 +33,39 @@ def test_benchmark_scalars(
         def run():
             reader = EventAccumulator(path)
             reader.Reload()
-            list(reader.Scalars("tag"))
+
+    else:
+        raise ValueError(f"Invalid {implementation=}")
+
+    benchmark(run)
+
+
+@pt.mark.benchmark
+@pt.mark.parametrize("implementation", ["this", "tensorboard"])
+@pt.mark.parametrize("n_samples", [100])
+@pt.mark.parametrize("imgsize", [224])
+def test_benchmark_images(
+    implementation: str, tmp_path: Path, n_samples: int, imgsize: int, benchmark
+):
+    outdir = tmp_path / "simple_scalar"
+    with tbx.SummaryWriter(str(outdir)) as writer:
+        for i in range(n_samples):
+            img = np.random.randint(0, 255, size=(imgsize, imgsize, 3), dtype=np.uint8)
+            writer.add_image("img", img, global_step=i, dataformats="HWC")
+
+    path = str(detect_tbfile(outdir))
+
+    if implementation == "this":
+
+        def run():
+            reader = SummaryReader(path)
+            list(reader)
+
+    elif implementation == "tensorboard":
+
+        def run():
+            reader = EventAccumulator(path)
+            reader.Reload()
 
     else:
         raise ValueError(f"Invalid {implementation=}")
